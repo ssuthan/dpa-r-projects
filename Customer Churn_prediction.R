7a1124530de49ae40356adeacbeeea8bfa8f46a5
ssuthan <67495556+ssuthan@users.noreply.github.com>
2020-06-27 21:47:28 +0530

    Update Customer Churn_prediction.R

--- Customer Churn_prediction.R
+++ Customer Churn_prediction.R
@@ -1,11 +1,8 @@
-#Load Churn Data from csv
+#Load Churn Data from csv file
 
 churnData = read.csv("Telco customer churn_pre processed data_v0.2.csv", header = TRUE)
 
-#churnData
-
-#Split dataset into Train data and test data 4/5 & 1/5
-
+#Split dataset into Train data and test data 80:20
 
 set.seed(1234)
 indx = sample(2, nrow(churnData), replace=TRUE, prob=c(0.8, 0.2)) #Split dataset 4/5 & 1/5
@@ -24,6 +21,8 @@ churnData.testLabels = churnData[indx==2, 21] #Copy from churn column where samp
 
 library(class) #Load library
 
+#Running knn algorithm with multiple k values
+
 #k=1
 churnData.pred1 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=1)
 churnData.pred1
@@ -32,16 +31,13 @@ churnData.pred1
 churnData.pred3 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=3)
 churnData.pred3
 
-
 #k=71
 churnData.pred71 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=71)
 churnData.pred71
 
-
 #k=80
 churnData.pred80 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=80)
 churnData.pred80
-
 
 #k=83
 churnData.pred83 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=83)
@@ -51,20 +47,18 @@ churnData.pred83
 churnData.pred85 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=85)
 churnData.pred85
 
-
 #k=87
 churnData.pred87 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=87)
 churnData.pred87
-
 
 #k=91
 churnData.pred91 = knn(train = churnData.train, test = churnData.test, cl = churnData.trainLabels, k=91)
 churnData.pred91
 
-#Cross tables
+#Cross tables for all of the above k values
 
 
-library(gmodels)
+library(gmodels) #Load gmodels library
 
 CrossTable(x = churnData.testLabels, y = churnData.pred1, prop.chisq=FALSE)
 CrossTable(x = churnData.testLabels, y = churnData.pred3, prop.chisq=FALSE)
@@ -74,25 +68,4 @@ CrossTable(x = churnData.testLabels, y = churnData.pred83, prop.chisq=FALSE)
 CrossTable(x = churnData.testLabels, y = churnData.pred85, prop.chisq=FALSE)
 CrossTable(x = churnData.testLabels, y = churnData.pred87, prop.chisq=FALSE)
 CrossTable(x = churnData.testLabels, y = churnData.pred91, prop.chisq=FALSE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
